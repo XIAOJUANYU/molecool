c0135b870ca93e29df95e4db5498fa34f4163e49
xiaojuan <yuxiaojuan89@gmail.com>
2020-01-26 04:12:00 +0800

    update with explanation

--- molecool/io/pdb.py
+++ molecool/io/pdb.py
@@ -1,16 +1,30 @@
 """
 
 """
-def open_pdb(f_loc):
+import numpy as np
+
+def open_pdb(file_location):
+    """
+    Open and read coordinates and symbols from a pdb file.
+
+    The pdb file must specify the atom elements in the last column, 
+    and follow the conventions outlline in the PDB format specification.
+
+    Parameters
+    ---------
+
+    """
     # This function reads in a pdb file and returns the atom names and coordinates.
-    with open(f_loc) as f:
+    with open(file_location) as f:
         data = f.readlines()
-    c = []
-    sym = []
-    for l in data:
-        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
-            sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
-            c.append(c2)
-    coords = np.array(c)
-    return sym, coords
+    coordinates = []
+    symbols = []
+    for line in data:
+        if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
+            sym.append(line[76:79].strip())
+            atom_coordinates = [float(x) for x in line[30:55].split()]
+            coordinates.append(atom_coordinates)
+
+    # convert list to numpy array
+    coordinates = np.array(coordinates)
+    return symbols, coordinates
--- molecool/measure.py
+++ molecool/measure.py
@@ -7,10 +7,33 @@ import numpy as np
 
 
 def calculate_distance(rA, rB):
+    """
+    Calculate the distance between two points
+
+    Parameters
+    ----------
+    rA, rB: np.adarray
+        The coordinates of each points.
+
+    Returns
+    -------
+    distance : float        
+        The distance between the two points.
+
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 1])
+    >>> r2 = np.array({0, 0.1, 0})
+    >>> calculate_distance(r1, r2)
+    0.1
+    """
+
+
+
     # This function calculates the distance between two points given as numpy arrays.
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
-    return dist
+    distance_vector = (rA - rB)
+    distance_scalar = np.linalg.norm(distance_vector)
+    return distance
 
 def calculate_angle(rA, rB, rC, degrees=False):
     # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
