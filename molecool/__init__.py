34c5a95e1ac916b8f5068dbd4808f6f15233fb12
xiaojuan <yuxiaojuan89@gmail.com>
2020-01-26 03:28:16 +0800

    structure to module

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -5,6 +5,12 @@ a python package for analyzing and virualizing xyz file for molssi best practice
 
 # Add imports here
 from .functions import *
+from .measure import calculate_angle, calculate_distance
+from .visulize import draw_molecule, bond_histogram
+from .molecule import build_bond_list
+from .atom_data import atomic_colors, atomic_weights
+
+import molecool.io
 
 # Handle versioneer
 from ._version import get_versions
---
+++ molecool/io/_init_.py
@@ -0,0 +1,5 @@
+"""
+IO sub
+"""
+from .pdb import open_pdb
+from .xyz import open_xyz, write_xyz
